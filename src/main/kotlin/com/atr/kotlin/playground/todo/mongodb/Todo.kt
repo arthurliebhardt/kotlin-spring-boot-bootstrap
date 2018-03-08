b7a86af2f28a3487e05c37256e15461121aede1f
Arthur Liebhardt <arthur.liebhardt@sap.com>
2018-03-08 23:04:38 +0100

    started with mongodb integration

---
+++ src/main/kotlin/com/atr/kotlin/playground/todo/mongodb/Todo.kt
@@ -0,0 +1,16 @@
+package com.atr.kotlin.playground.todo.mongodb
+
+import com.atr.kotlin.playground.todo.TodoValidationException
+import org.springframework.data.mongodb.core.mapping.Document
+import javax.persistence.Id
+
+@Document
+class Todo(@Id val id: String? = null,
+           var name: String = "", var completed: Boolean = false) {
+
+    fun validate() {
+        val errors = mutableListOf<String>()
+        if (name.isEmpty()) errors.add("Todo.model is empty")
+        if (errors.isNotEmpty()) throw TodoValidationException("Invalid Device", errors)
+    }
+}
